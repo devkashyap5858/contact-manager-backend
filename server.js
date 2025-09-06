ba8f1feb5e17931109382c07bb575a9db9e0006d
devkashyap5858 <dev.kashyap80045@gmail.com>
2025-09-06 21:57:17 +0530

    add cors for connect fronted to backend

--- server.js
+++ server.js
@@ -2,12 +2,18 @@ import express from 'express'
 import dotenv from "dotenv"
 dotenv.config({ path: './.env' });
 import errorHandler from './middleware/error.handler.js';
+import cors from "cors"
 
 const app = express()
 
 const port = process.env.PORT || 8000
 
 app.use(express.json());
+
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true
+}));
 
 
 //routes import
